5dbe6cc87e95de6eb1f45253ed59663b88fb7901
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-04-27 01:30:12 +0800

    add database mongoodb and connect to it

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import express from "express";
+import mongoose from "mongoose"
 
-console.log("hello")
 const PORT = 5000;
+const DB_URL = "mongodb+srv://user:user@cluster0.vg1vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const app = express();
 
+// подключаем для express преобразование JSON формата
 app.use(express.json())
 
 app.post("/", (req, res) => {
 	console.log(req.body);
-	res.status(200).json("ajajaja")
+	res.status(200).json("Сервер работает")
 })
 
-app.listen(PORT, () => console.log("SERVER IS WORKING " + PORT));
+async function startApp() {
+	try {
+		await mongoose.connect(DB_URL, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true
+		})
+		app.listen(PORT, () => console.log("SERVER IS WORKING " + PORT));
+	} catch (e) {
+		console.log(e)
+	}
+}
+startApp()
+
+// app.get("/sa", (req, res) => {
+// 	console.log(req.query.tru);
+// 	res.status(200).json("ajajaja")
+// })
+
+
 
