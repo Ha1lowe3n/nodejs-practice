42a190713d2247da3c0b1034797020a332bd2763
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-05-05 06:01:14 +0800

    add Post Schema

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose"
 
+import Post from "./Post.js";
+
 const PORT = 5000;
 const DB_URL = "mongodb+srv://user:user@cluster0.vg1vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
@@ -9,9 +11,14 @@ const app = express();
 // подключаем для express преобразование JSON формата
 app.use(express.json())
 
-app.post("/", (req, res) => {
-	console.log(req.body);
-	res.status(200).json("Сервер работает")
+app.post("/", async (req, res) => {
+	try {
+		const {author, title, content, picture} = req.body
+		const post = await Post.create({author, title, content, picture})
+		res.status(200).json(post)
+	} catch (err) {
+		res.status(500).json(err)
+	}
 })
 
 async function startApp() {
@@ -22,7 +29,7 @@ async function startApp() {
 		})
 		app.listen(PORT, () => console.log("SERVER IS WORKING " + PORT));
 	} catch (e) {
-		console.log(e)
+		console.error(e)
 	}
 }
 startApp()
